8b17fc8bfe784db31c0c71bc099bd78ef3797f5d
SANDIP CHITALE <sandipchitale@gmail.com>
2023-02-06 22:45:15 -0800

    .

--- src/index.ts
+++ src/index.ts
@@ -17,24 +17,38 @@ import * as child_process from 'child_process';
             kubectlPrompt.close();
             return;
         }
-
         if (command === 'history') {
             history.forEach((command: string) => {
                 console.log(command);
             });
             kubectlPrompt.prompt();
             return;
+        } else if (command.match(/^!\d+$/)) {
+            const matches = command.match(/^!(\d+)$/);
+            command = undefined;
+            if (matches && matches.length === 2) {
+                try {
+                    const commandNumber = parseInt(matches[1]);
+                    const historyCommand = history.find(historyItem => historyItem.startsWith(`${commandNumber} >`));
+                    if (historyCommand) {
+                        command = historyCommand.replace(`${commandNumber} > kubectl `, '');
+                    }
+                } catch (error: any) {
+                }
+            }
         }
 
         try {
-            try {
-                history.push(`${commandNumber} > kubectl ${command}`);
-                const commandResult = child_process.execSync(`kubectl ${command}`, { encoding: 'utf8' });
-                if (commandResult) {
-                    console.log(commandResult);
+            if (command !== undefined) {
+                try {
+                    history.push(`${commandNumber} > kubectl ${command}`);
+                    const commandResult = child_process.execSync(`kubectl ${command}`, { encoding: 'utf8' });
+                    if (commandResult) {
+                        console.log(commandResult);
+                    }
+                } catch (error: any) {
+                    console.error(error.stderr);
                 }
-            } catch (error: any) {
-                console.error(error.stderr);
             }
         } finally {
             commandNumber++;
